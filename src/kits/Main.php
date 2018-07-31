82aacece6ade8ff940f93ee1cf4ce9e19e75d3d3
Slurry <redcroniphpne@gmail.com>
2018-07-31 12:10:13 -0300

    Update Main.php

--- src/kits/Main.php
+++ src/kits/Main.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace kits;
+
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\event\Listener;
 use pocketmine\item\Item;
+
 class Main extends PluginBase{
     
     public function onEnable() {
@@ -16,22 +19,19 @@ class Main extends PluginBase{
       $this->getLogger()->Info("Kits Is Disabled!");
     }
   
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) {
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) :bool {
       if($sender instanceof Player) {
         switch($cmd->getName()) {
           case "starterkit":
           case "sk":
           $sender->addTitle("Getting Starter Kit!");
           $item = item::get(267,0,1);
-          $inv = $sender->getInventory();
           $item->setCustomName("Starter Sword");
+          $inv = $sender->getInventory();
           $inv->addItem($item);
-  }
-   else 
-          {
-          $sender->sendMessage("A error acured. please go to https://github.com/joshuasing/StarterKit-PMMP for more info. Error Code: 1");
-          return true;
+          $sender->sendMessage("Kit...");
+          }
       }
+     return true;
     }
-  }
 }
